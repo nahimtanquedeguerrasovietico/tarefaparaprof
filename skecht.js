573d4275542f41e7a4bd2fd80facfce30bbfdf77
nahimtanquedeguerrasovietico <141925460+nahimtanquedeguerrasovietico@users.noreply.github.com>
2023-09-21 09:03:07 -0300

    aicionano archivo css y js

---
+++ skecht.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(400, 400);
+    background("rgb(175,175,175)");
+  }
+  
+  function draw() {
+    stroke("rgb(41,41,104)");
+  fill("rgb(12,240,153)");
+  
+    if (mouseIsPressed) {
+      circle
+    (mouseX, mouseY, 20, 35);
+     
+  
+    }
+  }
+  
